b5f7024838b61ed43e5c9f5e42b59dc22a965643
Nigialro <112541578+Nigialro@users.noreply.github.com>
2022-11-29 16:35:49 +0100

    Le Mauvais Coin

--- src/services/CityService.php
+++ src/services/CityService.php
@@ -2,12 +2,16 @@
 
 namespace App\services;
 
+//use App\services\Contracts\HttpClient;
+
+
 // API postal to INSEE : https://api.gouv.fr/documentation/api_carto_codes_postaux
 // API INSEE to data : https://api.gouv.fr/documentation/api-geo
 // EP =================== /commune/{code}
 
 // HTPP CLIENT
 // https://symfony.com/doc/current/http_client.html
+
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -16,53 +20,92 @@ class CityService
 
     /**
      * @var HttpClientInterface
+     * App\services\Contracts\HttpClient;
      */
     private HttpClientInterface $client;
+
 
     public function __construct(HttpClientInterface $client)
     {
         $this->client = $client;
+
     }
 
-    public function getCity(string $postalCode): array
+    public function normalizer(string $input): string
     {
-        // on check si le nom de la ville correspond a un des element de la reponse
-        // de la premiére API, si non on prend le premier element du tableau
-        // $resp[0]
+        // Aurillac => aurillac => auRillac => aurillaC
+        // Saint-Flour => Saint Flour => saint flour => saint,flour => saint-flour
+        // todo : enlever les accent, passer tout en strtolower, remove [a-z0-9] => ' '
 
-        $ukn = [
+        $search  = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ò', 'ó', 'ô', 'õ', 'ö', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', ' ', '-', '_', "'", '/', ',');
+        $replace = array('a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', '', '', '', '', 'sur', '');
+        $input = str_replace($search, $replace, $input);
+
+        return $input;
+    }
+
+    public function getCity(string $postalCode, string $city): array
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://apicarto.ign.fr/api/codes-postaux/communes/' . $postalCode
+        );
+        //  'https://geo.api.gouv.fr/communes?codePostal=$postalCode'
+
+        //$statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        //$contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        //$content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $contentData = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+
+        if(!is_array($contentData)){
+            throw new \RuntimeException('bad format data');
+        }
+
+
+        $cityFound = null;
+        $normalizedCity = $this->normalizer($city);
+        // todo un foreach $contentData et check si on a la ville, si on trouve alors on stock dans $cityFound
+        // foreach
+        foreach($contentData as $ligne) {
+            $normalizedData = $this->normalizer($ligne['nomCommune']);
+            if($normalizedCity == $normalizedData) {
+                $cityFound = $city;
+            }
+        }
+        // ensuite
+        if($cityFound === null){
+            $cityFound = $contentData[0] ?? null;
+        }
+
+        if($cityFound === null){
+            throw new \RuntimeException('no data found');
+        }
+
+        $codeInsee = $cityFound['codeCommune'];
+
+        /// todo 2eme call API
+
+        $urlApiCommune = 'https://geo.api.gouv.fr/communes/' . $codeInsee
+            . '?fields=nom,code,codesPostaux,siren,codeEpci,codeDepartement,codeRegion,population,departement&format=json&geometry=centre';
+
+
+
+        // todo si jamais la reponse est bonne alors on return la reponse de l'api
+
+        // todo si non alors on return UKN
+
+        return  [
             'city' => 'unknown',
             'cp' => 'unknown',
             'lat' => '0',
             'lon' => '0',
             'departement' => 'unknown',
         ];
-        $response = $this->client->request(
-            'GET',
-            'https://geo.api.gouv.fr/communes?codePostal=$postalCode'
-        );
-
-        $statusCode = $response->getStatusCode();
-        // $statusCode = 200
-        $contentType = $response->getHeaders()['application/json'][0];
-        // $contentType = 'application/json'
-        $content = $response->getContent();
-        // $content = '{
-        //    "nom": "Versailles",
-        //    "code": "78646",
-        //    "codeDepartement": "78",
-        //    "siren": "217806462",
-        //    "codeEpci": "247800584",
-        //    "codeRegion": "11",
-        //    "codesPostaux": [
-        //      "78000"
-        //    ],
-        //    "population": 84808
-        //  }'
-        $content = $response->toArray();
-        // $content = ['nom' => 'Versailles'', 'code' => '78645', ...]
-
-        return $content ?? $ukn;
     }
+
 
 }
